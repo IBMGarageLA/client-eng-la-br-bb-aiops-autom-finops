ea20fd9702139d04f1740c733282879f60a38d7b
deivson rayner teixeira da costa <drayner@MacBook-Pro-de-deivson.local>
2023-03-16 17:54:50 -0300

    fixed index issue

--- IBM_Cloud/singleVM-VPC/main.tf
+++ IBM_Cloud/singleVM-VPC/main.tf
@@ -54,11 +54,11 @@ data "ibm_is_ssh_key" "cam_sshkey" {
 }
 
 #Create SSHKey
-resource "ibm_is_ssh_key" "cam_sshkey" {
-  count = var.public_ssh_key !=""?1:0 
-  name       = var.ssh_name
-  public_key = var.public_ssh_key
-}
+#resource "ibm_is_ssh_key" "cam_sshkey" {
+#  count = var.public_ssh_key !=""?1:0 
+#  name       = var.ssh_name
+#  public_key = var.public_ssh_key
+#}
 
 #Create VSI
 resource "ibm_is_instance" "cam-server" {

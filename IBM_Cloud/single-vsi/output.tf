457e71db14327fe80f979714146b421c64f73694
deivson rayner teixeira da costa <drayner@MacBook-Pro-de-deivson.local>
2023-03-21 12:30:59 -0300

    added vsi v2 resource

---
+++ IBM_Cloud/single-vsi/output.tf
@@ -0,0 +1,17 @@
+# server Floating IP address
+output "server_interface_id" {
+  value = ibm_is_instance.cam-server.primary_network_interface[0].id
+}
+
+# server private IP address
+output "server_private_ip_address" {
+  value = ibm_is_instance.cam-server.primary_network_interface[0].primary_ipv4_address
+}
+
+output "server_name" {
+  value = ibm_is_instance.cam-server.name
+}
+
+output "server_profile" {
+  value = ibm_is_instance.cam-server.profile
+}
--- IBM_Cloud/ssh-key/output.tf
+++ IBM_Cloud/ssh-key/output.tf
@@ -5,3 +5,7 @@ output "ssh_key_id" {
   value = var.public_ssh_key!=""?one(ibm_is_ssh_key.cam_sshkey[*].id):one(data.ibm_is_ssh_key.cam_sshkey[*].id)
 }
 
+output "ssh_key_name" {
+  value = var.public_ssh_key!=""?one(ibm_is_ssh_key.cam_sshkey[*].name):one(data.ibm_is_ssh_key.cam_sshkey[*].name)
+}
+
